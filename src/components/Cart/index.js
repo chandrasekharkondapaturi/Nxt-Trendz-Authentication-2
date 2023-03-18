37a7ee72f2dd44e7066fec7e9499f1b90a90bfec
chandrasekharkondapaturi <chandu.mc87>
2023-03-18 04:34:15 +0000

    nxtTrendzAuthentication

---
+++ src/components/Cart/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="cart-img"
+      />
+    </div>
+  </>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="products-cont">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="products-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
